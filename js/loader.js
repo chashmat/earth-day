8de164abd1b74211ef0a6498984f5d8aaaa2abab
Chashmat un Nasim <chashmatnasim@gmail.com>
2021-04-25 17:06:46 +0500

    Added loader

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    let loaderBg = document.getElementsByClassName("loader")[0];
+    let loader = document.getElementsByClassName("loader-mov")[0];
+    loader.classList.add("mov");
+    loaderBg.classList.add("bg");
+});
